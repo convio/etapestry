3def4ded61c2aca8a6fff2efcf19800e2f0feadf
hmcgowan <hugh_mcgowan@yahoo.com>
2012-07-31 16:42:48 -0500

    logout at exit to stop the login from failing on multiple sessions

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,3 +8,7 @@ require 'watirmark/cucumber/env'
 Watirmark::Configuration.instance.bypass_admin_login = false
 
 include ETapestry
+
+at_exit {
+  LoginView.logout_link.click if LoginView.logout_link.present?
+}
