3e7ba36b1036bc411436d1023aa140ed54d6626b
hmcgowan <hugh_mcgowan@yahoo.com>
2012-08-22 13:34:52 -0500

    add rakefile to trigger test cases from hudson

---
+++ rakefile.rb
@@ -0,0 +1,22 @@
+$: << File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
+
+begin
+  sh "bundle install" if File.dirname(__FILE__) =~ /\/(hudson|mnt)\/workspace\//
+rescue Gem::RemoteFetcher::FetchError
+  puts "Unable to connect - retrying #{Time.now}"
+  retry
+end
+
+require 'bundler/setup'
+require 'cucumber/rake/task'
+
+module RakeHelper
+  def self.cucumber_task(task_name, files=nil)
+    Cucumber::Rake::Task.new(task_name) do |t|
+      t.cucumber_opts = "-r features #{FileList[files]}"
+    end
+  end
+end
+
+RakeHelper.cucumber_task(:account, "features/account/**/*.feature")
+RakeHelper.cucumber_task(:login, "features/login/**/*.feature")
