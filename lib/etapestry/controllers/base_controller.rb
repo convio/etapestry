755080a285eb5c20d3f21e3ce0627a0db0d600dd
hmcgowan <hugh_mcgowan@yahoo.com>
2012-11-13 15:06:43 -0600

    add waiter to make sure save button is enabled

--- lib/etapestry/controllers/base_controller.rb
+++ lib/etapestry/controllers/base_controller.rb
@@ -3,6 +3,7 @@ module ETapestry
     def submit
       alert = Page.browser.close_alert do
         if @view.save_button.exists?
+          Watir::Wait.until {@view.save_button.enabled?}
           @view.save_button.click
         else
           raise Watirmark::TestError, "No submit button found or defined for this form"
